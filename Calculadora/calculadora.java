41849f34dec3d506cb119601341fbfa0c25cfab9
Rui de Gouveia Soares Neto <ruigsn@edu.unifor.br>
2022-06-04 23:19:24 -0300

    Criei a classe calculadora
    Criação da classe calculadora com as declarações de função: soma, subtração, divisão, multiplicação, exponencial, raiz quadrada, piso e teto.

---
+++ Calculadora/calculadora.java
@@ -0,0 +1,36 @@
+package Calculadora;
+
+public class calculadora {
+    
+    public void soma() {
+        
+    }
+
+    public void subtracao() {
+        
+    }
+
+    public void multiplicacao() {
+        
+    }
+
+    public void divisao() {
+        
+    }
+
+    public void exponencial() {
+        
+    }
+
+    public void raizQuadrada() {
+        
+    }
+
+    public void piso() {
+        
+    }
+
+    public void teto() {
+        
+    }
+}
