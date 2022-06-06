05d8ee343252b514ecbf6aa36f2c087a74a14d90
pc <rafastb@gmail.com>
2022-06-06 08:45:56 -0300

    Implementação de dois metodos (Soma e Subtração)

--- Calculadora/calculadora.java
+++ Calculadora/calculadora.java
@@ -1,36 +1,41 @@
 package Calculadora;
 
 public class calculadora {
-    
-    public void soma() {
-        
+
+    public void soma(int numero1, int numero2) {
+        int resultadoSoma;
+        resultadoSoma = numero1 + numero2;
+        System.out.println("Resultado da operação de soma é: " + resultadoSoma);
     }
 
-    public void subtracao() {
-        
+    public void subtracao(int numero1, int numero2) {
+        int resultadoSubtracao;
+        resultadoSubtracao = numero1 - numero2;
+        System.out.println("Resultado da operação de subtração é: " + resultadoSubtracao);
+
     }
 
     public void multiplicacao() {
-        
+
     }
 
     public void divisao() {
-        
+
     }
 
     public void exponencial() {
-        
+
     }
 
     public void raizQuadrada() {
-        
+
     }
 
     public void piso() {
-        
+
     }
 
     public void teto() {
-        
+
     }
 }
