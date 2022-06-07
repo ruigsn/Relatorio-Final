f96f78745c1be4f3ffd07dae20d355173fe4bd25
Rui de Gouveia Soares Neto <ruigsn@edu.unifor.br>
2022-06-07 14:06:02 -0300

    Correção da retirada da multiplicação

--- Calculadora/calculadora.java
+++ Calculadora/calculadora.java
@@ -12,6 +12,11 @@ public class calculadora {
         return resultado;
     }
 
+    public double multiplicacao(double numero1, double numero2) {
+        double resultado = numero1 * numero2;
+        return resultado;
+    }
+
     public double divisao(double numero1, double numero2) {
         double resultado = numero1 / numero2;
         return resultado;
